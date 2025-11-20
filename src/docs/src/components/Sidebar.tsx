38a73939775dcfc4fd779b0d73507e40f02b72da
Peter Adelodun <adelodunpeter69@gmail.com>
2025-11-20 13:33:22 +0100

    Fix sidebar to always show on desktop screens

--- src/docs/src/components/Sidebar.tsx
+++ src/docs/src/components/Sidebar.tsx
@@ -37,9 +37,9 @@ function Sidebar({ activeSection, onSectionChange, isOpen, onClose }: SidebarPro
       </AnimatePresence>
       
       <motion.aside
-        className="w-72 bg-gray-50 dark:bg-gray-800 border-r border-gray-200 dark:border-gray-700 h-[calc(100vh-64px)] md:h-[calc(100vh-72px)] flex flex-col fixed left-0 top-[64px] md:top-[72px] z-50 lg:h-[calc(100vh-124px)] lg:top-[124px] lg:translate-x-0"
-        initial={false}
-        animate={{ x: isOpen ? 0 : -288 }}
+        className="w-72 bg-gray-50 dark:bg-gray-800 border-r border-gray-200 dark:border-gray-700 h-[calc(100vh-64px)] md:h-[calc(100vh-72px)] flex flex-col fixed left-0 top-[64px] md:top-[72px] z-50 lg:h-[calc(100vh-124px)] lg:top-[124px]"
+        style={{ x: 0 }}
+        animate={{ x: isOpen || window.innerWidth >= 1024 ? 0 : -288 }}
         transition={{ type: 'spring', damping: 25, stiffness: 200 }}
       >
         <div className="px-6 py-6 border-b border-gray-200 dark:border-gray-700 flex items-center justify-between">
